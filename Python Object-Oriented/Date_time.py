fc742cfd8fe2f7dc9e7679db4b8373e17efdf7d1
Antony Micheal <starkantony356@gmail.com>
2022-11-27 23:19:13 +0530

    update

---
+++ Python Object-Oriented/Date_time.py
@@ -0,0 +1,11 @@
+import datetime
+x = datetime.datetime.now()
+
+print(x)
+print(x.hour)
+print(x.day)
+print(x.year)
+print(x.month)
+print(x.strftime("%A"))
+print(x.strftime("%B"))
+print(x.strftime("%D"))
